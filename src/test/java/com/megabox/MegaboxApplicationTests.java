e7a609053aa65d4818810d9e64413f4df870a93b
DESKTOP-U99AA5T\jglor <jglorys@naver.com>
2021-10-15 21:29:13 +0900

    초기 환경세팅

---
+++ src/test/java/com/megabox/MegaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
