343fa5edda9fa33ea7c1d4d0fb4649e12b825927
DESKTOP-U99AA5T\jglor <jglorys@naver.com>
2021-11-12 20:37:10 +0900

    aws webMvcConfig

--- src/main/java/com/megabox/config/WebMvcConfig.java
+++ src/main/java/com/megabox/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")	// http://localhost/images/... 으로 연결하겠다
-		.addResourceLocations("file:///C:\\Users\\jglor\\웹개발\\6_spring_project\\megabox\\images\\admin/"); 
+		.addResourceLocations("file:////home/ec2-user/upload_images/"); 
 		//  실제 파일 저장 위치
 		// "file:///"  맨 앞에 붙이기  ,   맨뒤에 "/"붙이기! - FileManagerService의 ADMIN_FILE_UPLOAD_PATH와 일치해야 함
 	}
