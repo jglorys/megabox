c521b2ca9dd5c9456eef83eba48e025151a40979
DESKTOP-U99AA5T\jglor <jglorys@naver.com>
2022-01-24 21:45:32 +0900

    Revert "AWS 설정 해제"
    This reverts commit 3de020997ed7f75d242e800c2b1a1b1435f3a0c3.

--- src/main/java/com/megabox/MegaboxApplication.java
+++ src/main/java/com/megabox/MegaboxApplication.java
@@ -2,9 +2,10 @@ package com.megabox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MegaboxApplication{
+public class MegaboxApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MegaboxApplication.class, args);
