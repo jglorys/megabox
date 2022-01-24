3de020997ed7f75d242e800c2b1a1b1435f3a0c3
DESKTOP-U99AA5T\jglor <jglorys@naver.com>
2022-01-24 21:44:55 +0900

    AWS 설정 해제

--- src/main/java/com/megabox/MegaboxApplication.java
+++ src/main/java/com/megabox/MegaboxApplication.java
@@ -2,10 +2,9 @@ package com.megabox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MegaboxApplication extends SpringBootServletInitializer {
+public class MegaboxApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MegaboxApplication.class, args);
--- src/main/java/com/megabox/config/WebMvcConfig.java
+++ src/main/java/com/megabox/config/WebMvcConfig.java
@@ -10,8 +10,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	//실제 저장된 파일과 이미지 패스를 매핑해줌
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/images/**")	// http://localhost/images/... 으로 연결하겠다
-		.addResourceLocations("file:/home/ec2-user/upload_images/"); 
+		registry.addResourceHandler("/images/**")
+		.addResourceLocations("file:///C:\\Users\\jglor\\웹개발\\6_spring_project\\megabox\\images\\admin/");
+//		.addResourceLocations("file:/home/ec2-user/upload_images/"); 
 		//  실제 파일 저장 위치
 		// "file:///"  맨 앞에 붙이기  ,   맨뒤에 "/"붙이기! - FileManagerService의 ADMIN_FILE_UPLOAD_PATH와 일치해야 함
 	}
